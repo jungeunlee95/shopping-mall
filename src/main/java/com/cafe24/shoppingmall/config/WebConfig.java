5d5360ecf68b0c5fbd17984ad55d7cd6dfdbec43
jungeunlee95 <leeap1004@gmail.com>
2019-07-09 20:21:02 +0900

    view resolver 설정

--- src/main/java/com/cafe24/shoppingmall/config/WebConfig.java
+++ src/main/java/com/cafe24/shoppingmall/config/WebConfig.java
@@ -3,10 +3,14 @@ package com.cafe24.shoppingmall.config;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
+
+import com.cafe24.config.web.MVCConfig;
 
 @Configurable
 @EnableAspectJAutoProxy
 @ComponentScan({"com.cafe24.shoppingmall.hello.controller"})
+@Import({MVCConfig.class})
 public class WebConfig {
 
 }
--- src/main/java/com/cafe24/shoppingmall/hello/controller/HelloController.java
+++ src/main/java/com/cafe24/shoppingmall/hello/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/hello")
 	public String hello() { 
-		return "/WEB-INF/views/hello/hello.jsp";
+		return "hello/hello";
 	}
 }
  
