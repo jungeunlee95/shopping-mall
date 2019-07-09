92a656dda4bb5b937a5cf9b77734b4e0614d4901
jungeunlee95 <leeap1004@gmail.com>
2019-07-09 19:40:04 +0900

    Spring project 최초 설정

---
+++ src/main/java/com/cafe24/shoppingmall/hello/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.cafe24.shoppingmall.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    
+    @RequestMapping("/hello")
+	public String hello() { 
+		return "/WEB-INF/views/hello/hello.jsp";
+	}
+}
+ 
